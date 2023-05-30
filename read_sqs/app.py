09c20ecfbe957f117321822cde1a960a7535ee86
wallas <wagratom22@gmail.com>
2023-05-30 15:54:28 -0300

    Salving in table

--- read_sqs/app.py
+++ read_sqs/app.py
@@ -2,15 +2,47 @@ from chalice import Chalice
 import boto3
 import json
 
-ENDPOINT_URL_SEND ='http://host.docker.internal:4566'
-ENDPOINT_URL_RECEIVE = "http://host.docker.internal:4566/_aws/sqs/messages"
-QUEUE_URL = "http://queue.localhost.localstack.cloud:4566/000000000000"
+ENDPOINT_INTERNAL ='http://host.docker.internal:4566'
+ENDPOINT_MSG_SQS = "http://host.docker.internal:4566/_aws/sqs/messages"
+ENDPOINT_QUEUE = "http://queue.localhost.localstack.cloud:4566/000000000000"
 QUEUE_NAME = 'passwords'
+TABLE_NAME = 'users'
 
 app = Chalice(app_name='ww')
 app.debug = True
 
-def check_pass_word(password):
+################################################################################
+# 									getts
+################################################################################
+
+
+def get_data_SQS(body):
+	app.log.info("GETTS PASSWORD FROM SQS")
+	app.log.debug("Body: %s", body)
+
+	return body['Login'], body['Senha'],
+
+def get_dynamodb(dynamo_name):
+	app.log.info("GETTS DYNAMODB TABLE: %s", dynamo_name)
+	dynamo_client = boto3.resource('dynamodb', endpoint_url=ENDPOINT_INTERNAL)
+	return dynamo_client.Table(dynamo_name)
+
+def saving_user_in_db(body):
+	table = get_dynamodb(TABLE_NAME)
+
+	app.log.info("Saving Item in DynamoDB")
+	app.log.debug("Item: %s", body)
+
+	login, password = get_data_SQS(body)
+	response = table.put_item(
+		Item={
+			'Login': login,
+			'Senha': password
+			}
+		)
+	app.log.info("Success in create user")
+
+def check_valid_password(password):
 	if len(password) < 8:
 		return False
 	if not any(char.isdigit() for char in password):
@@ -23,24 +55,18 @@ def check_pass_word(password):
 		return False
 	return True
 
-def get_password_SQS(event):
-	for record in event:
-		SQS = boto3.client("sqs", endpoint_url=ENDPOINT_URL_SEND)
-		password = record.body
-		return password
-
 @app.on_sqs_message(queue=QUEUE_NAME)
 def on_event(event):
 	try:
 		for record in event:
-			password = get_password_SQS(event)
-			app.log.debug("Password: %s", password)
-			if check_pass_word(password):
-				app.log.debug("Password Valida")
+			body_json = json.loads(record.body)
+			login, password = get_data_SQS(body_json)
+			if check_valid_password(password):
+				saving_user_in_db(body_json)
 			else:
 				app.log.debug("Senha errada rapa")
 	except Exception as e:
-		print(str(e))
+		app.log.debug(str(e))
 
 # app.log.debug("Received message with contents: %s", vars(record))
 # app.log.debug("Received message with contents wagratom: %s", record.eventSourceARN)
@@ -53,3 +79,4 @@ def on_event(event):
 # awslocal sqs send-message --queue-url http://queue.localhost.localstack.cloud:4566/000000000000/passwords --message-body "transforma"
 
 
+#awslocal dynamodb delete-table --table-name users
