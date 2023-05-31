433d867a8fc70103938c379d30d9e0a6444a207f
wallas <wagratom22@gmail.com>
2023-05-31 13:50:47 -0300

    create interfase

--- sqs_teste/app.py
+++ sqs_teste/app.py
@@ -1,32 +1,17 @@
-from chalice import Chalice
+from chalice import Chalice, Response
 import boto3
 import json
+import os
+import sys
 
-ENDPOINT_URL_SEND ='http://host.docker.internal:4566'
-ENDPOINT_URL_RECEIVE = "http://host.docker.internal:4566/_aws/sqs/messages"
+# ENDPOINT_URL_RECEIVE = "http://host.docker.internal:4566/_aws/sqs/messages"
+ENDPOINT_INTERNAL ='http://host.docker.internal:4566'
 QUEUE_URL = "http://queue.localhost.localstack.cloud:4566/000000000000"
 QUEUE_NAME = 'passwords'
+TABLE_NAME = 'users'
 
 app = Chalice(app_name='ww')
-
-@app.lambda_function()
-def receive_message_sqs(event, context):
-	SQS = boto3.client("sqs", endpoint_url=ENDPOINT_URL_RECEIVE)
-	response = SQS.receive_message(QueueUrl=f"{QUEUE_URL}/{QUEUE_NAME}")
-	if 'Messages' in response:
-		body = response['Messages'][0]['Body']
-		return  body
-	else:
-		return {'body': 'No messages found'}
-
-# @app.lambda_function()
-# def send_message(event, context):
-# 	sqs = boto3.client('sqs', endpoint_url=ENDPOINT_URL_SEND)
-# 	response = sqs.send_message(
-# 		QueueUrl=f'{QUEUE_URL}/{QUEUE_NAME}',
-# 		MessageBody='Hello World2!'
-# 	)
-# 	return "Success"
+app.debug = True
 
 """
 # Acessar informações da requisição
@@ -37,25 +22,56 @@ def receive_message_sqs(event, context):
 	body		 | current_request.json_bod
 """
 
-# envia para a fila sqs
-def send_message(password):
+################################################################################
+# 									responses
+################################################################################
+
+
+def get_dynamodb(dynamo_name):
+	app.log.info("GETTS DYNAMODB TABLE: %s", dynamo_name)
+	dynamo_client = boto3.resource('dynamodb', endpoint_url=ENDPOINT_INTERNAL)
+	return dynamo_client.Table(dynamo_name)
+
+@app.route('/cadastro/{login}', methods=['GET'])
+def get_response(login):
 	try:
-		sqs = boto3.client('sqs', endpoint_url=ENDPOINT_URL_SEND)
+		table = get_dynamodb(TABLE_NAME)
+		response = table.get_item(
+			Key={
+				'Login': login
+			}
+		)
+		item = response['Item']
+		app.log.debug("Item: %s", item)
+		message = item['Message']
+		app.log.debug("Message: %s", message)
+		return message
+	except Exception as e:
+		app.log.error("Error in get response: %s", str(e))
+
+def send_message(body):
+	try:
+		sqs = boto3.client('sqs', endpoint_url=ENDPOINT_INTERNAL)
 		response = sqs.send_message(
 			QueueUrl=f'{QUEUE_URL}/{QUEUE_NAME}',
-			MessageBody=str(password)
+			MessageBody=str(body)
 		)
-		return "Seu login está sendo processado, aguarde..."
 	except Exception as e:
 		return (str(e))
+
+@app.route('/index', methods=['GET'])
+def index():
+	with open('./index.html') as f:
+		content =  f.read()
+	headers = {'Content-Type': 'text/html'}
+	return Response(body=content, headers=headers)
+
 
 @app.route('/cadastro', methods=['POST'])
 def receive_data():
 	try:
-		paraments = app.current_request.json_body
-		name = paraments['Name']
-		password = paraments['PassWord']
-		return send_message(password)
+		body = app.current_request.json_body
+		send_message(body)
+		return f"Seu login está sendo processado, Você pode acompanhar o status em: /cadastro/{(body['Login'])}"
 	except Exception as e:
 		return (str(e))
-
