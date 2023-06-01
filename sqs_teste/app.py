ba7236228cfb8985ef786bf0b87ac9967fa23170
wallas <wagratom22@gmail.com>
2023-06-01 14:53:32 -0300

    Push Quase pronto

--- sqs_teste/app.py
+++ sqs_teste/app.py
@@ -3,9 +3,7 @@ import boto3
 import json
 import os
 import sys
-from time import sleep as time
 
-# ENDPOINT_URL_RECEIVE = "http://host.docker.internal:4566/_aws/sqs/messages"
 ENDPOINT_INTERNAL ='http://host.docker.internal:4566'
 QUEUE_URL = "http://queue.localhost.localstack.cloud:4566/000000000000"
 QUEUE_NAME = 'passwords'
@@ -14,19 +12,60 @@ TABLE_NAME = 'users'
 app = Chalice(app_name='ww')
 app.debug = True
 
-"""
-# Acessar informações da requisição
-	http_method  | current_request.method
-	headers	  | current_request.headers
-	query_params | current_request.query_params
-	path_params  | current_request.path_params
-	body		 | current_request.json_bod
-"""
+################################################################################
+# 									Index
+################################################################################
+@app.route('/', methods=['GET'])
+def index():
+	with open('./index.html') as f:
+		content =  f.read()
+	headers = {'Content-Type': 'text/html'}
+	return Response(body=content, headers=headers)
 
 ################################################################################
-# 									responses
+# 									Register
+################################################################################
+@app.route('/register', methods=['GET'])
+def register():
+	with open('./register.html') as f:
+		content =  f.read()
+	headers = {'Content-Type': 'text/html'}
+	return Response(body=content, headers=headers)
+
+################################################################################
+# 									Cadastro
 ################################################################################
 
+@app.route('/cadastro', methods=['POST'])
+def receive_data():
+	try:
+		body = app.current_request.json_body
+		send_message(body)
+		login = body['Login']
+		return get_response(login)
+	except Exception as e:
+		return (str(e))
+
+def send_message(body):
+	try:
+		sqs = boto3.client('sqs', endpoint_url=ENDPOINT_INTERNAL)
+		response = sqs.send_message(
+			QueueUrl=f'{QUEUE_URL}/{QUEUE_NAME}',
+			MessageBody=str(body)
+		)
+	except Exception as e:
+		return (str(e))
+
+def get_response(login):
+	try:
+		status = get_message_password(login)
+		status = {'message': status}
+		set_messege_null(login)
+		app.log.debug("Message: %s", status)
+		return Response(body=status, status_code=200)
+	except Exception as e:
+		message = {'message': 'Login ainda não consta no banco de dados, tente novamente mais tarde'}
+		return Response(body=message, status_code=400)
 
 def get_dynamodb(dynamo_name):
 	app.log.info("GETTS DYNAMODB TABLE: %s", dynamo_name)
@@ -50,54 +89,3 @@ def get_message_password(login):
 		}
 	)
 	return response['Item']['Message']
-
-@app.route('/cadastro/{login}', methods=['GET'])
-def get_response(login):
-	try:
-		status = get_message_password(login)
-		set_messege_null(login)
-		app.log.debug("Message: %s", status)
-		return {'message': status}
-	except Exception as e:
-		return {'message': 'login não encontrado'}
-
-def send_message(body):
-	try:
-		sqs = boto3.client('sqs', endpoint_url=ENDPOINT_INTERNAL)
-		response = sqs.send_message(
-			QueueUrl=f'{QUEUE_URL}/{QUEUE_NAME}',
-			MessageBody=str(body)
-		)
-	except Exception as e:
-		return (str(e))
-
-@app.route('/register', methods=['GET'])
-def register():
-	with open('./register.html') as f:
-		content =  f.read()
-	headers = {'Content-Type': 'text/html'}
-	return Response(body=content, headers=headers)
-
-@app.route('/', methods=['GET'])
-def index():
-	with open('./index.html') as f:
-		content =  f.read()
-	headers = {'Content-Type': 'text/html'}
-	return Response(body=content, headers=headers)
-
-@app.route('/cadastro', methods=['POST'])
-def receive_data():
-	try:
-		body = app.current_request.json_body
-		send_message(body)
-		response = {
-			'message': f"Seu login está sendo processado. Você pode acompanhar o status em: /cadastro/{body['Login']}"
-		}
-		redirect_url = f"cadastro/{body['Login']}"
-		headers = {'Location': redirect_url}
-		return Response(body=response, status_code=302, headers=headers)
-	except Exception as e:
-		return (str(e))
-
-
-# @app.route('/fluxograma.png/', methods=['GET'])
