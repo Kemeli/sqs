33a1535efdecbb5f2175f26e710ff1b4e180f8b2
wallas <wagratom22@gmail.com>
2023-06-02 12:52:03 -0300

    upgrade site

--- sqs_teste/app.py
+++ sqs_teste/app.py
@@ -6,9 +6,11 @@ import sys
 
 ENDPOINT_INTERNAL ='http://host.docker.internal:4566'
 QUEUE_URL = "http://queue.localhost.localstack.cloud:4566/000000000000"
-QUEUE_NAME = 'passwords'
-TABLE_NAME = 'users'
 
+S3_BUCKET = 'resource'
+QUEUE_NAME = 'passwords'
+
+s3_client = boto3.client('s3', endpoint_url=ENDPOINT_INTERNAL)
 app = Chalice(app_name='ww')
 app.debug = True
 
@@ -17,20 +19,20 @@ app.debug = True
 ################################################################################
 @app.route('/', methods=['GET'])
 def index():
-	with open('./index.html') as f:
-		content =  f.read()
+	response = s3_client.get_object(Bucket=S3_BUCKET, Key='index.html')
+	page = response['Body'].read().decode('utf-8')
 	headers = {'Content-Type': 'text/html'}
-	return Response(body=content, headers=headers)
+	return Response(body=page, status_code=200, headers=headers)
 
 ################################################################################
 # 									Register
 ################################################################################
 @app.route('/register', methods=['GET'])
 def register():
-	with open('./register.html') as f:
-		content =  f.read()
+	response = s3_client.get_object(Bucket=S3_BUCKET, Key='register.html')
+	page = response['Body'].read().decode('utf-8')
 	headers = {'Content-Type': 'text/html'}
-	return Response(body=content, headers=headers)
+	return Response(body=page, status_code=200, headers=headers)
 
 ################################################################################
 # 									Cadastro
@@ -41,8 +43,6 @@ def receive_data():
 	try:
 		body = app.current_request.json_body
 		send_message(body)
-		login = body['Login']
-		return get_response(login)
 	except Exception as e:
 		return (str(e))
 
@@ -55,37 +55,3 @@ def send_message(body):
 		)
 	except Exception as e:
 		return (str(e))
-
-def get_response(login):
-	try:
-		status = get_message_password(login)
-		status = {'message': status}
-		set_messege_null(login)
-		app.log.debug("Message: %s", status)
-		return Response(body=status, status_code=200)
-	except Exception as e:
-		message = {'message': 'Login ainda não consta no banco de dados, tente novamente mais tarde'}
-		return Response(body=message, status_code=400)
-
-def get_dynamodb(dynamo_name):
-	app.log.info("GETTS DYNAMODB TABLE: %s", dynamo_name)
-	dynamo_client = boto3.resource('dynamodb', endpoint_url=ENDPOINT_INTERNAL)
-	return dynamo_client.Table(dynamo_name)
-
-def set_messege_null(login):
-	table = get_dynamodb(TABLE_NAME)
-	response = table.put_item(
-		Item={
-			'Login': login,
-			'Message': 'NULL'
-			}
-		)
-
-def get_message_password(login):
-	table = get_dynamodb(TABLE_NAME)
-	response = table.get_item(
-		Key={
-			'Login': login
-		}
-	)
-	return response['Item']['Message']
